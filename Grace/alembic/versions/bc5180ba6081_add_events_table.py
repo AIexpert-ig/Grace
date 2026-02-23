231ac65125803dcac06319d3fc75323b00ec60ef
WA ANGIELSKI <ahmed.backerjr@gmail.com>
2026-02-24 00:22:12 +0400

    feat: Add Alembic migration script for an events table.

---
+++ Grace/alembic/versions/bc5180ba6081_add_events_table.py
@@ -0,0 +1,24 @@
+"""add events table
+
+Revision ID: bc5180ba6081
+Revises: 001_escalations
+Create Date: 2026-02-24 00:19:36.374100
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bc5180ba6081'
+down_revision = '001_escalations'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
