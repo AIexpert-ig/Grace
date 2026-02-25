61bad6eba16b474347b42e83583b0829dd609ced
WA ANGIELSKI <ahmed.backerjr@gmail.com>
2026-02-25 20:43:15 +0400

    Simplify start.sh

--- Grace/start.sh
+++ Grace/start.sh
@@ -1,19 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 
-echo "Requirements file content:"
-cat requirements.txt
-cd /opt/render/project/src
-
-echo "Python executable:"
-python -c "import sys; print(sys.executable)"
-
-echo "Alembic installed?"
-python -m pip show alembic || true
-
-echo "Running migrations..."
-python -m pip install alembic
+echo "Running database migrations..."
 python -m alembic upgrade head
 
-echo "Starting server..."
-exec python -m uvicorn app.main:app --host 0.0.0.0 --port "${PORT:-10000}"
+echo "Starting uvicorn server..."
+exec python -m uvicorn app.main:app --host 0.0.0.0 --port "${PORT}"
